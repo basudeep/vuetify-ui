b555a36f43497ef9df5344804afe1d3fddd16be9
basudeep <deepbasu1997@gmail.com>
2020-09-01 23:42:50 +0530

    reload get error

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
+const app = express();
 
-app = express();
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
-var port = process.env.PORT || 8080;
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
 
-app.listen(port);
-
-console.log('server started '+ port);
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
