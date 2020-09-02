693893c589a2a1d0d8c30d49c7e320332a8e4817
basudeep <deepbasu1997@gmail.com>
2020-09-02 11:34:41 +0530

    check

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
   
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var history = require('connect-history-api-fallback')
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-app = express();
+const app = express();
 
-app.use('/', serveStatic(__dirname + "/dist"));
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-var port = process.env.PORT || 8080;
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
-app.listen(port);
-app.use(history())
-console.log('server started '+ port);
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
